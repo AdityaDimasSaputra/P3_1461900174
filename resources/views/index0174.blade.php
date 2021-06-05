4c014ce32f7f4092a8741f7ca450c59bf4c13163
AdityaDimasSaputra <adityadmss@gmail.com>
2021-06-05 12:57:07 +0700

    Mengerjakan

--- resources/views/index0174.blade.php
+++ resources/views/index0174.blade.php
@@ -4,29 +4,31 @@
 <title>1461900174</title>
 </head>
 <body>
-<h3>Data User</h3>
-<p>Cari :</p>
-<form action="/pasien/cari" method="GET">
-<input type="text" name="lihat" placeholder="Cari pasien .." value="{{ old('cari') }}">
-<input type="submit" value="CARI">
+<center><h10>DATA USER PASIEN</h10></center>
+<center><p>Cari :</p></center>
+<center><form action="/pasien/cari" method="GET"></center>
+<center><input type="text" name="lihat" placeholder="Cari Pasien .." value="{{ old('cari') }}"><center>
+<br/>
+<center><input type="submit" value="CARI"></center>
 </form>
 <br>
-<form action="/pasien/kamar" method="GET">
-<input type="text" name="lihat" placeholder="Cari Kamar" value="{{ old('cari') }}">
-<input type="submit" value="CARI">
+<center><form action="/pasien/kamar" method="GET"></center>
+<center><input type="text" name="lihat" placeholder="Cari Kamar" value="{{ old('cari') }}"></center>
+<br/><center><input type="submit" value="CARI"></center>
+<br/>
 </form>
-
-<a href="/pasien/tambah"> + User Baru </a>
+<br/>
+<center><a href="/pasien/tambah"> Tambah User Baru </a></center>
 <br/>
 <br/>
-<table border="1">
+<center><table border="4">
 <tr>
 <th>Id</th>
 <th>Nama</th>
 <th>Alamat</th>
-</tr>
+</tr></center>
 @foreach($pasien as $p)
-<tr>
+<center><tr>
 <td>{{ $p->id }}</td>
 <td>{{ $p->nama }}</td>
 <td>{{ $p->alamat }}</td>
@@ -35,7 +37,7 @@
 |
 <a href="/pasien/hapus/{{ $p->id }}">Hapus</a>
 </td>
-</tr>
+</tr></center>
 @endforeach
 </table>
 </body>
